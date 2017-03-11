73fbe277a5a3eff1ef090c9fe8eb72eec3308f6f
M. Shulhan <ms@kilabit.info>
2017-03-11 22:46:20 +0700

    ints: various enhancements imported from floats64
    * IntsCount(): new function to count an occurence of value in slice
    * IntsCounts(): new function to count list of class from list of slice
    * IntsInsertionSort(): function to sort slice of int using insertion-sort
    * IntsSortByIndex(): function to sort slice of int by list of sorted index
    * IntsInplaceMergesort(): function to sort slice with merge-sort algorithm
    * IntsIndirectSort(): function sort slice and return the sorted index

--- ints.go
+++ ints.go
@@ -1,4 +1,4 @@
-// Copyright 2016 Mhd Sulhan <ms@kilabit.info>. All rights reserved.
+// Copyright 2016-2017 M. Shulhan <ms@kilabit.info>. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -6,9 +6,9 @@ package numerus
 
 //
 // IntsFindMax given slice of integer, return the maximum value in slice and
-// index of maximum value.
+// its index.
 //
-// If data is empty, return -1 in value and index, and false in ok.
+// If data is empty, it will return `-1` in value and index, and false in ok.
 //
 // Example, given a slice of data: [0 1 2 3 4], it will return 4 as max and 4
 // as index of maximum value.
@@ -35,9 +35,9 @@ func IntsFindMax(d []int) (maxv int, maxi int, ok bool) {
 
 //
 // IntsFindMin given slice of integer, return the minimum value in slice and
-// index of minimum value.
+// its index.
 //
-// If data is empty, return -1 in value and index.
+// If data is empty, return -1 in value and index, and false in ok.
 //
 // Example, given a slice of data: [0 1 2 3 4], it will return 0 as min and 0
 // as minimum index.
@@ -73,6 +73,45 @@ func IntsSum(d []int) (sum int) {
 }
 
 //
+// IntsCount will count number of class in data.
+//
+func IntsCount(d []int, class int) (count int) {
+	if len(d) <= 0 {
+		return
+	}
+
+	for _, v := range d {
+		if v == class {
+			count++
+		}
+	}
+	return count
+}
+
+//
+// IntsCounts will count class in data and return each of the counter.
+//
+// For example, if data is "[1,1,2]" and class is "[1,2]", this function will
+// return "[2,1]".
+//
+//	idx class  count
+//	0 : 1   -> 2
+//	1 : 2   -> 1
+//
+func IntsCounts(d, classes []int) (counts []int) {
+	if len(classes) <= 0 {
+		return
+	}
+
+	counts = make([]int, len(classes))
+
+	for x, c := range classes {
+		counts[x] = IntsCount(d, c)
+	}
+	return
+}
+
+//
 // IntsMaxCountOf will count number of occurence of each element of classes
 // in data and return the class with maximum count.
 //
@@ -81,8 +120,10 @@ func IntsSum(d []int) (sum int) {
 // If classes has the same count value, then the first max in the class will be
 // returned.
 //
-// For example, given a data [0, 1, 0, 1, 0] and classes [0, 1], the function
-// will count 0 as 3, 1 as 2; and return 0.
+// For example, given a data [5, 6, 5, 6, 5] and classes [5, 6, 7], the
+// function will count 5 as 3, 6 as 2, and 7 as 0.
+// Since frequency of 5 is greater than 6 and 7, then it will return `5` and
+// `true`.
 //
 func IntsMaxCountOf(d, classes []int) (int, bool) {
 	if len(classes) == 0 {
@@ -92,17 +133,12 @@ func IntsMaxCountOf(d, classes []int) (int, bool) {
 		return -2, false
 	}
 
-	counts := make([]int, len(classes))
-
-	for x, c := range classes {
-		for _, d := range d {
-			if c == d {
-				counts[x]++
-			}
-		}
-	}
+	counts := IntsCounts(d, classes)
 
 	_, maxi, _ := IntsFindMax(counts)
+	if maxi < 0 {
+		return -1, false
+	}
 
 	return classes[maxi], true
 }
@@ -114,6 +150,9 @@ func IntsSwap(d []int, x, y int) {
 	if x == y {
 		return
 	}
+	if len(d) <= 1 || x > len(d) || y > len(d) {
+		return
+	}
 
 	tmp := d[x]
 	d[x] = d[y]
@@ -121,8 +160,8 @@ func IntsSwap(d []int, x, y int) {
 }
 
 //
-// IntsIsExist check if integer value exist in slice of integer, return true if
-// exist, false otherwise.
+// IntsIsExist will return true if value `v` exist in slice of `d`,
+// otherwise it will return false.
 //
 func IntsIsExist(d []int, i int) bool {
 	for _, v := range d {
@@ -142,4 +181,228 @@ func IntsTo64(ints []int) []int64 {
 		i64[x] = int64(v)
 	}
 	return i64
+}
+
+//
+// IntsInsertionSort will sort the data using insertion-sort algorithm.
+//
+// Parameters:
+// - `data` is slice that will be sorted.
+// - `idx` is indices of data.
+// - `l` is starting index of slice to be sorted.
+// - `r` is end index of slice to be sorted.
+//
+func IntsInsertionSort(d, ids []int, l, r int, asc bool) {
+	for x := l; x < r; x++ {
+		for y := x + 1; y < r; y++ {
+			if asc {
+				if d[x] > d[y] {
+					IntsSwap(ids, x, y)
+					IntsSwap(d, x, y)
+				}
+			} else {
+				if d[x] < d[y] {
+					IntsSwap(ids, x, y)
+					IntsSwap(d, x, y)
+				}
+			}
+		}
+	}
+}
+
+//
+// IntsSortByIndex will sort the slice `d` using sorted index `sortedIds`.
+//
+func IntsSortByIndex(d *[]int, sortedIds []int) {
+	newd := make([]int, len(*d))
+
+	for i := range sortedIds {
+		newd[i] = (*d)[sortedIds[i]]
+	}
+
+	(*d) = newd
+}
+
+//
+// IntsInplaceMergesort in-place merge-sort without memory allocation.
+//
+func IntsInplaceMergesort(d []int, idx []int, l, r int, asc bool) {
+	// (0) If data length == Threshold, then
+	if l+SortThreshold >= r {
+		// (0.1) use insertion sort.
+		IntsInsertionSort(d, idx, l, r, asc)
+		return
+	}
+
+	// (1) Divide into left and right.
+	res := (r + l) % 2
+	c := (r + l) / 2
+	if res == 1 {
+		c++
+	}
+
+	// (2) Sort left.
+	IntsInplaceMergesort(d, idx, l, c, asc)
+
+	// (3) Sort right.
+	IntsInplaceMergesort(d, idx, c, r, asc)
+
+	// (4) Merge sorted left and right.
+	if asc {
+		if d[c-1] <= d[c] {
+			// (4.1) If the last element of the left is lower then
+			// the first element of the right, i.e. [1 2] [3 4];
+			// no merging needed, return immediately.
+			return
+		}
+	} else {
+		if d[c-1] >= d[c] {
+			return
+		}
+	}
+
+	intsInplaceMerge(d, idx, l, c, r, asc)
+}
+
+//
+// Let `x` be the first index of left-side, `y` be the first index of
+// the right-side, and `r` as length of slice `d`
+//
+func intsInplaceMerge(d []int, idx []int, x, y, r int, asc bool) {
+	var ylast int
+
+	// (4.3) Loop until either x or y reached the maximum slice.
+	for x < r && y < r {
+		// (4.3.1) IF DATA[x] <= DATA[y]
+		if asc {
+			if d[x] <= d[y] {
+				x++
+
+				// (4.3.1.2) IF x > y THEN GOTO 4.3
+				if x >= y {
+					goto next
+				}
+
+				// (4.3.1.3) GOTO 4.3
+				continue
+			}
+		} else {
+			if d[x] >= d[y] {
+				x++
+
+				if x >= y {
+					goto next
+				}
+
+				continue
+			}
+		}
+
+		// (4.3.2) LET YLAST := the next DATA[y] that is less DATA[x]
+		ylast = intsMoveY(d, x, y, r, asc)
+
+		// (4.3.3) SWAP DATA, X, Y, YLAST
+		intsMultiswap(d, idx, x, y, ylast)
+
+	next:
+		// (4.3.4) LET Y := the minimum value between x and r on `d`
+		intsMinY(d, &x, &y, r, asc)
+	}
+}
+
+// (4.3.4) LET Y := the minimum value between x and r on `d`
+func intsMinY(d []int, x, y *int, r int, asc bool) {
+	for *x < r {
+		if asc {
+			*y = intsMin(d, *x, r)
+		} else {
+			*y = intsMax(d, *x, r)
+		}
+
+		if *y != *x {
+			break
+		}
+		(*x)++
+	}
+}
+
+func intsMoveY(d []int, x, y, r int, asc bool) int {
+	yorg := y
+	y++
+	for y < r {
+		if asc {
+			if d[y] >= d[x] {
+				break
+			}
+			if d[y] < d[yorg] {
+				break
+			}
+		} else {
+			if d[y] <= d[x] {
+				break
+			}
+			if d[y] > d[yorg] {
+				break
+			}
+		}
+		y++
+	}
+	return y
+}
+
+func intsMultiswap(d []int, idx []int, x, y, ylast int) int {
+	for y < ylast {
+		IntsSwap(idx, x, y)
+		IntsSwap(d, x, y)
+		x++
+		y++
+		if y >= ylast {
+			return y
+		}
+		if d[x] <= d[y] {
+			return y
+		}
+	}
+
+	return y
+}
+
+func intsMin(d []int, l, r int) (m int) {
+	min := d[l]
+	m = l
+	for l++; l < r; l++ {
+		if d[l] <= min {
+			min = d[l]
+			m = l
+		}
+	}
+	return
+}
+
+func intsMax(d []int, l, r int) (m int) {
+	maxv := d[l]
+	m = l
+	for l++; l < r; l++ {
+		if d[l] >= maxv {
+			maxv = d[l]
+			m = l
+		}
+	}
+	return m
+}
+
+//
+// IntsIndirectSort will sort the data and return the sorted index.
+//
+func IntsIndirectSort(d []int, asc bool) (sortedIdx []int) {
+	dlen := len(d)
+
+	sortedIdx = make([]int, dlen)
+	for i := 0; i < dlen; i++ {
+		sortedIdx[i] = i
+	}
+
+	IntsInplaceMergesort(d, sortedIdx, 0, dlen, asc)
+
+	return
 }
--- ints_test.go
+++ ints_test.go
@@ -1,10 +1,11 @@
-// Copyright 2016 Mhd Sulhan <ms@kilabit.info>. All rights reserved.
+// Copyright 2016-2017 M. Shulhan <ms@kilabit.info>. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package numerus_test
 
 import (
+	"fmt"
 	"github.com/shuLhan/numerus"
 	"testing"
 )
@@ -14,6 +15,19 @@ var (
 		{},
 		{5, 6, 7, 8, 9, 0, 1, 2, 3, 4},
 		{0, 1, 0, 1, 0, 1, 0, 1, 0},
+		{1, 1, 2, 2, 3, 1, 2},
+	}
+	dIntsSorted = [][]int{
+		{},
+		{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+		{0, 0, 0, 0, 0, 1, 1, 1, 1},
+		{1, 1, 1, 2, 2, 2, 3},
+	}
+	dIntsSortedDesc = [][]int{
+		{},
+		{9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
+		{1, 1, 1, 1, 0, 0, 0, 0, 0},
+		{3, 2, 2, 2, 1, 1, 1},
 	}
 )
 
@@ -55,6 +69,55 @@ func TestIntsSum(t *testing.T) {
 	assert(t, 45, got, true)
 }
 
+func TestIntsCount(t *testing.T) {
+	got := numerus.IntsCount(dInts[0], 0)
+
+	assert(t, 0, got, true)
+
+	got = numerus.IntsCount(dInts[1], 1)
+
+	assert(t, 1, got, true)
+
+	got = numerus.IntsCount(dInts[2], 1)
+
+	assert(t, 4, got, true)
+
+	got = numerus.IntsCount(dInts[3], 0)
+
+	assert(t, 0, got, true)
+
+	got = numerus.IntsCount(dInts[3], 3)
+
+	assert(t, 1, got, true)
+}
+
+func TestIntsCountsEmpty(t *testing.T) {
+	classes := []int{1, 2, 3}
+	exp := []int{0, 0, 0}
+
+	got := numerus.IntsCounts(dInts[0], classes)
+
+	assert(t, exp, got, true)
+}
+
+func TestIntsCountsEmptyClasses(t *testing.T) {
+	classes := []int{}
+	var exp []int
+
+	got := numerus.IntsCounts(dInts[1], classes)
+
+	assert(t, exp, got, true)
+}
+
+func TestIntsCounts(t *testing.T) {
+	classes := []int{1, 2, 3}
+	exp := []int{3, 3, 1}
+
+	got := numerus.IntsCounts(dInts[3], classes)
+
+	assert(t, exp, got, true)
+}
+
 func TestIntsMaxCountOf(t *testing.T) {
 	classes := []int{0, 1}
 	exp := int(0)
@@ -67,6 +130,56 @@ func TestIntsMaxCountOf(t *testing.T) {
 	got, _ = numerus.IntsMaxCountOf(dInts[2], classes)
 
 	assert(t, exp, got, true)
+}
+
+func TestIntsSwapEmpty(t *testing.T) {
+	exp := []int{}
+
+	numerus.IntsSwap(dInts[0], 1, 6)
+
+	assert(t, exp, dInts[0], true)
+}
+
+func TestIntsSwapEqual(t *testing.T) {
+	in := make([]int, len(dInts[1]))
+	copy(in, dInts[1])
+
+	exp := make([]int, len(in))
+	copy(exp, in)
+
+	numerus.IntsSwap(in, 1, 1)
+
+	assert(t, exp, in, true)
+}
+
+func TestIntsSwapOutOfRange(t *testing.T) {
+	in := make([]int, len(dInts[1]))
+	copy(in, dInts[1])
+
+	exp := make([]int, len(in))
+	copy(exp, in)
+
+	numerus.IntsSwap(in, 1, 100)
+
+	assert(t, exp, in, true)
+}
+
+func TestIntsSwap(t *testing.T) {
+	in := make([]int, len(dInts[1]))
+	copy(in, dInts[1])
+
+	exp := make([]int, len(in))
+	copy(exp, in)
+
+	numerus.IntsSwap(in, 0, len(in)-1)
+
+	assert(t, exp, in, false)
+
+	tmp := exp[0]
+	exp[0] = exp[len(exp)-1]
+	exp[len(exp)-1] = tmp
+
+	assert(t, exp, in, true)
 }
 
 func TestIntsIsExist(t *testing.T) {
@@ -88,4 +201,185 @@ func TestIntsIsExist(t *testing.T) {
 		s = numerus.IntsIsExist(d, 10)
 		assert(t, false, s, true)
 	}
+}
+
+func TestIntsInsertionSort(t *testing.T) {
+	for x := range dInts {
+		d := make([]int, len(dInts[x]))
+
+		copy(d, dInts[x])
+
+		ids := make([]int, len(d))
+		for x := range ids {
+			ids[x] = x
+		}
+
+		numerus.IntsInsertionSort(d, ids, 0, len(ids), true)
+
+		assert(t, dIntsSorted[x], d, true)
+	}
+}
+
+func TestIntsInsertionSortDesc(t *testing.T) {
+	for x := range dInts {
+		d := make([]int, len(dInts[x]))
+
+		copy(d, dInts[x])
+
+		ids := make([]int, len(d))
+		for x := range ids {
+			ids[x] = x
+		}
+
+		numerus.IntsInsertionSort(d, ids, 0, len(ids), false)
+
+		assert(t, dIntsSortedDesc[x], d, true)
+	}
+}
+
+func TestIntsSortByIndex(t *testing.T) {
+	ids := [][]int{
+		{},
+		{5, 6, 7, 8, 9, 0, 1, 2, 3, 4},
+		{0, 2, 4, 6, 8, 1, 3, 5, 7},
+		{0, 1, 5, 6, 2, 3, 4},
+	}
+
+	for x := range dInts {
+		d := make([]int, len(dInts[x]))
+
+		copy(d, dInts[x])
+
+		numerus.IntsSortByIndex(&d, ids[x])
+
+		assert(t, dIntsSorted[x], d, true)
+	}
+}
+
+var intsInSorts = [][]int{
+	{9, 8, 7, 6, 5, 4, 3},
+	{9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
+	{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+	{0, 6, 7, 8, 5, 1, 2, 3, 4, 9},
+	{9, 8, 7, 6, 5, 4, 3, 2, 1},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{51, 50, 56, 55, 55, 58, 55, 55, 58, 56,
+		57, 50, 56, 59, 62, 60, 49, 63, 61, 56,
+		58, 67, 61, 59, 60, 49, 56, 52, 61, 64,
+		70, 57, 65, 69, 57, 64, 62, 66, 63, 62,
+		54, 67, 61, 57, 55, 60, 30, 66, 57, 60,
+		68, 60, 61, 63, 58, 58, 56, 57, 60, 69,
+		69, 64, 63, 63, 67, 65, 58, 63, 64, 67,
+		59, 72, 63, 63, 65, 71, 67, 76, 73, 64,
+		67, 74, 60, 68, 65, 64, 67, 64, 65, 69,
+		77, 67, 72, 77, 72, 77, 61, 79, 77, 68,
+		62},
+}
+
+var intsExpSorts = [][]int{
+	{3, 4, 5, 6, 7, 8, 9},
+	{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+	{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+	{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+	{1, 2, 3, 4, 5, 6, 7, 8, 9},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{30, 49, 49, 50, 50, 51, 52, 54, 55, 55,
+		55, 55, 55, 56, 56, 56, 56, 56, 56, 57,
+		57, 57, 57, 57, 57, 58, 58, 58, 58, 58,
+		58, 59, 59, 59, 60, 60, 60, 60, 60, 60,
+		60, 61, 61, 61, 61, 61, 61, 62, 62, 62,
+		62, 63, 63, 63, 63, 63, 63, 63, 63, 64,
+		64, 64, 64, 64, 64, 64, 65, 65, 65, 65,
+		65, 66, 66, 67, 67, 67, 67, 67, 67, 67,
+		67, 68, 68, 68, 69, 69, 69, 69, 70, 71,
+		72, 72, 72, 73, 74, 76, 77, 77, 77, 77,
+		79},
+}
+
+var intsExpSortsDesc = [][]int{
+	{9, 8, 7, 6, 5, 4, 3},
+	{9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
+	{9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
+	{9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
+	{9, 8, 7, 6, 5, 4, 3, 2, 1},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{79, 77, 77, 77, 77, 76, 74, 73, 72, 72,
+		72, 71, 70, 69, 69, 69, 69, 68, 68, 68,
+		67, 67, 67, 67, 67, 67, 67, 67, 66, 66,
+		65, 65, 65, 65, 65, 64, 64, 64, 64, 64,
+		64, 64, 63, 63, 63, 63, 63, 63, 63, 63,
+		62, 62, 62, 62, 61, 61, 61, 61, 61, 61,
+		60, 60, 60, 60, 60, 60, 60, 59, 59, 59,
+		58, 58, 58, 58, 58, 58, 57, 57, 57, 57,
+		57, 57, 56, 56, 56, 56, 56, 56, 55, 55,
+		55, 55, 55, 54, 52, 51, 50, 50, 49, 49,
+		30},
+}
+
+func TestIntsIndirectSort(t *testing.T) {
+	var res, exp string
+
+	for i := range intsInSorts {
+		numerus.IntsIndirectSort(intsInSorts[i], true)
+
+		res = fmt.Sprint(intsInSorts[i])
+		exp = fmt.Sprint(intsExpSorts[i])
+
+		assert(t, exp, res, true)
+	}
+}
+
+func TestIntsIndirectSortDesc(t *testing.T) {
+	var res, exp string
+
+	for i := range intsInSorts {
+		numerus.IntsIndirectSort(intsInSorts[i], false)
+
+		res = fmt.Sprint(intsInSorts[i])
+		exp = fmt.Sprint(intsExpSortsDesc[i])
+
+		assert(t, exp, res, true)
+	}
+}
+
+func TestIntsIndirectSort_Stability(t *testing.T) {
+	exp := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	got := numerus.IntsIndirectSort(intsInSorts[5], true)
+
+	assert(t, exp, got, true)
+}
+
+func TestIntsIndirectSortDesc_Stability(t *testing.T) {
+	exp := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	got := numerus.IntsIndirectSort(intsInSorts[5], false)
+
+	assert(t, exp, got, true)
+}
+
+func TestIntsInplaceMergesort(t *testing.T) {
+	size := len(intsInSorts[6])
+	idx := make([]int, size)
+
+	numerus.IntsInplaceMergesort(intsInSorts[6], idx, 0, size, true)
+
+	assert(t, intsExpSorts[6], intsInSorts[6], true)
+}
+
+func TestIntsIndirectSort_SortByIndex(t *testing.T) {
+	expIds := []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
+	in1 := []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
+	in2 := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	exp := fmt.Sprint(in1)
+
+	sortedIds := numerus.IntsIndirectSort(in1, true)
+
+	assert(t, expIds, sortedIds, true)
+
+	// Reverse the sort.
+	numerus.IntsSortByIndex(&in2, sortedIds)
+
+	got := fmt.Sprint(in2)
+
+	assert(t, exp, got, true)
 }
